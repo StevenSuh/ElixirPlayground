e3d413cac8eff043013ecd9b304c096b0388b148
Steven Suh <stevenesuh@gmail.com>
2018-10-07 14:57:27 -0700

    Topic REST endpoints

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -4,7 +4,8 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic
 
   def index(conn, _params) do
-    topics = Repo.all(Topic)
+    topics = Enum.sort(Repo.all(Topic),
+      fn(x, y) -> x.id < y.id end)
     render conn, "index.html", topics: topics
   end
 
@@ -16,14 +17,48 @@ defmodule Discuss.TopicController do
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
-      {:ok, post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: topic_path(conn, :index))
       {:error, changeset} ->
         conn
         |> put_flash(:error, "Can't be blank")
-        |> redirect(to: topic_path(conn, :new))
+        |> render "new.html", changeset: changeset
     end
+  end
+
+  def edit(conn, params) do
+    %{"id" => topic_id} = params
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, params) do
+    %{"id" => topic_id, "topic" => new_topic} = params
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, new_topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Can't be blank")
+        |> render "edit.html", changeset: changeset, topic: old_topic
+    end
+  end
+
+  def delete(conn, params) do
+    %{"id" => topic_id} = params
+    Repo.get!(Topic, topic_id) |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
   end
 end
